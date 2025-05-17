a55cbed3e1548382c3af53b25ac3126ebbdd84c6
zainul43 <ahmadmizan99@guru.smk.belajar.id>
2025-05-17 09:24:00 +0700

    add to master

---
+++ public/js/script.js
@@ -0,0 +1,1 @@
+alert ('welcome to SMKN 43 Jakarta');
---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>About</h1>
+    <a href="about">About</a>
+    <a href="home">Home</a>
+    <script src="js/script.js"></script>
+    <img src="img/post-laravel-12.png" alt="" srcset="" width="400px">
+</body>
+</html>
